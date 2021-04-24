b476d80ff737b59d63b728ed59f54a7412feae4a
Juan Romero <jrg89@hotmail.es>
2021-04-24 17:06:19 +0200

    start point

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-
-      </header>
+      <h2>Hello Disney + Clone</h2>
     </div>
   );
 }
